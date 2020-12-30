ed9627182b5db00b7c13bbe2275649fd46ba0ef9
GeorgeSato <satojogi8@gmail.com>
2020-12-30 14:25:24 -0500

    intento appnodekafka consumer

--- producer.js
+++ producer.js
@@ -24,8 +24,8 @@ producer.on('error', function (err) {
 })
 
 
-let averageDelay = 30000;  // in miliseconds
-let spreadInDelay = 20000; // in miliseconds
+let averageDelay = 300000;  // in miliseconds
+let spreadInDelay = 200000; // in miliseconds
 var delay = averageDelay + (Math.random() - 0.5) * spreadInDelay;
 
 
